9add183cbe2730b6cd10d1f99ff2e888504ec12e
Nicole Obretincheva <nobretincheva@gmail.com>
2024-07-15 20:36:08 +0100

    Update user_config.py for Dual Prompting

--- models/user_config.py
+++ models/user_config.py
@@ -3,12 +3,14 @@ from enum import Enum
 from models.baseline_fill_mask_model import FillMaskModel
 from models.baseline_generation_model import GenerationModel
 from models.baseline_llama_3_chat_model import Llama3ChatModel
+from models.dual_llama_3_model import Llama3DualPrompt
 
 
 class Models(Enum):
     BASELINE_FILL_MASK = "baseline_fill_mask"
     BASELINE_GENERATION = "baseline_generation"
     BASELINE_LLAMA_3_CHAT = "baseline_llama_3_chat"
+    DUAL_LLAMA_3 = "dual_llama_3_chat"
 
     # Add more models here
 
@@ -21,5 +23,7 @@ class Models(Enum):
             return GenerationModel
         elif model == Models.BASELINE_LLAMA_3_CHAT:
             return Llama3ChatModel
+        elif model == Models.DUAL_LLAMA_3:
+            return Llama3DualPrompt
         else:
             raise ValueError(f"Model `{model_name}` not found.")
